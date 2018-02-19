8ab25bcce840be8fd6e9e88e8819cbdf990f98b8
kom <kom@dockerhost.local>
2018-02-19 15:15:43 -0800

    Initial code for network library. More implementation needed before testing.

---
+++ config/defaults.py
@@ -0,0 +1,14 @@
+# This module contains all of the default
+# settings when net-harvest starts up.
+#
+# Author: kodavx86
+# Created: 02.19.2018
+
+# Default Ping timeout
+PING_TIMEOUT = 2
+
+# Default Ping retry
+PING_RETRY = 1
+
+# TCP connection timeout
+TCP_TIMEOUT = 2
